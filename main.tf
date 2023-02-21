cd20dba958c0b9b2c849597c14a1d3ac9f0130b3
Bùi Thế Lực <buitheluc@MacBook-Pro-cua-Bui.local>
2023-02-21 14:58:04 +0700

    feature/20230221/updateTerraform

--- main.tf
+++ main.tf
@@ -1,26 +1,101 @@
-terraform {
-  required_version = "~>1.3.0"
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
 
-required_providers {
-    aws = {
-      version = "~>4.28"
-      source  = "hashicorp/aws"
-    }
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "10.0.0.0/16"
 
-    newrelic = {
-      source  = "newrelic/newrelic"
-      version = "~>3.0"
-    }
+  tags = {
+    Name = "Some Custom VPC"
   }
 }
 
-provider "aws" {
-  region = "ap-northeast-1"
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.custom_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
-  default_tags {
-    tags = {
-      ServiceName = local.service_name
-      Env         = local.env
-    }
+  tags = {
+    Name = "Some Public Subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.custom_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Some Private Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "some_ig" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name = "Some Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.some_ig.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.some_ig.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_1_rt_a" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "web_sg" {
+  name   = "HTTP and SSH"
+  vpc_id = aws_vpc.custom_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_instance" {
+  ami           = "ami-0533f2ba8a1995cf9"
+  instance_type = "t2.nano"
+
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    "Name" : "LucBt"
   }
 }
--- network.tf
+++
@@ -1,9 +0,0 @@
-module "network" {
-  source = "./modules/network"
-  name   = local.name
-  env    = local.env
-
-  region   = data.aws_region.current.name
-  vpc_cidr = local.cidr["vpc"]
-  azs      = local.azs
-}
